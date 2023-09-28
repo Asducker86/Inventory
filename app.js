293b69104a0071f42392e358fccb87352a66360b
Asducker86 <144636571+Asducker86@users.noreply.github.com>
2023-09-28 11:34:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-// Define the API endpoint (URL)
-const apiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1LhsBoKPW9jYj3K5aghHSodcBRksrVI8adjAObPPode0/values/DBS%20Vehicle%20Database%20-%20Ancaster?key=YOUR_API_KEY';
-
 // Define Google API key and OAuth client ID
 const googleConfig = {
   'apiKey': 'AIzaSyCCvGMyNwWVk6-G6HB9esAFqm775hV07qc', // Your Google API key
   'clientId': '668016298289-o64rt3fb9vuptue6mvp47v67no840ptg.apps.googleusercontent.com', // Your Google OAuth client ID
 };
 
-// Function to fetch data from the API
-async function fetchData() {
+// Function to initialize the Google API client
+function initClient() {
+  gapi.client.init({
+    'apiKey': googleConfig.apiKey,
+    'clientId': googleConfig.clientId,
+    'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly',
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(() => {
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  });
+}
+
+// Function to update UI based on sign-in status
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    fetchDataFromSheets();
+  } else {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+}
+
+// Function to fetch data from Google Sheets
+async function fetchDataFromSheets() {
   try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data;
+    const response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1LhsBoKPW9jYj3K5aghHSodcBRksrVI8adjAObPPode0', // Your Google Sheet ID
+      range: 'DBS Vehicle Database - Ancaster', // Replace with your sheet name
+    });
+    const data = response.result;
+    displayData(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
+    console.error('Error fetching data:', error.result.error.message);
   }
 }
 
 // Function to display data on the page
 function displayData(data) {
-  // Replace this part with your logic to display the data on your webpage
-  // For example, you can update the DOM with the retrieved data
   const resultsDiv = document.getElementById('results');
   resultsDiv.innerHTML = '<h2>Vehicle Inventory</h2>';
 
@@ -39,12 +55,10 @@ function displayData(data) {
   }
 }
 
-// Fetch data when the page loads
-window.addEventListener('load', () => {
-  fetchData()
-    .then(displayData)
-    .catch((error) => {
-      // Handle errors
-      console.error('An error occurred:', error);
-    });
-});
+// Load the Google API client and authenticate the user
+function loadClient() {
+  gapi.load('client', initClient);
+}
+
+// Call the function to load the client and authenticate the user when the page loads
+window.addEventListener('load', loadClient);
